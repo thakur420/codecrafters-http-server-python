28eb95b208f4571a4558c5afd169251d7a864430
Indrajeet Thakur <38977906+thakur420@users.noreply.github.com>
2024-06-24 16:01:30 +0530

    extract request path

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 # Uncomment this to pass the first stage
 import socket
-
+def extract_path(request):
+    request_lines = request.splitlines()
+    if request_lines:
+        request_line = request_lines[0]
+        parts = request_line.split()
+        if len(parts) > 1:
+            part = parts[1]
+            return part
+    return None
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,9 +16,16 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221))
     client_socket, addr = server_socket.accept() # wait for client
-    client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
-
+    # print(f"Connected by {addr}")
+    request = client_socket.recv(1024).decode("utf-8")
+    # print(f"Request : {request}")
+    path = extract_path(request)
+    if path == "/":
+        client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client_socket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        
 if __name__ == "__main__":
     main()
