aca2ffbb78f6d46295b81be9fe2b8064798a9288
Indrajeet Thakur <38977906+thakur420@users.noreply.github.com>
2024-06-25 18:39:30 +0530

    concurrent connection

--- app/main.py
+++ app/main.py
@@ -1,49 +1,26 @@
-# Uncomment this to pass the first stage
 import socket
-from pathlib import Path
-def extract_header(request,header):
-    response = [line for line in request.splitlines() if header in line][0]
-    if len(response) > 0 :
-        return response.split(':')[-1].strip()
-    return None
+import time
+import concurrent.futures as cf
 
-def extract_body(request,path):
-    # print(path)
-    if "/echo" in path :
-        val = path.split("/")[-1]
-        return val if val != "echo" else "" 
-    if "/user-agent" in path:
-        return extract_header(request,"User-Agent")
-    if len(path) == 1 and "/" in path:
-        return ""
-    return None
+def handle_client(client_socket):
+    try :
+        request = client_socket.recv(1024).decode("utf-8")
+        print("processing client request")
+        time.sleep(10)
+        client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        client_socket.close()
 
-def extract_path(request):
-    request_lines = request.splitlines()
-    if request_lines:
-        request_line = request_lines[0]
-        parts = request_line.split()
-        if len(parts) > 1:
-            return parts[1]
-        return ""
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    server_socket = socket.create_server(("localhost", 4221))
-    client_socket, addr = server_socket.accept() # wait for client
-    # print(f"Connected by {addr}")
-    request = client_socket.recv(1024).decode("utf-8")
-    # print(f"Request : {request}")
-    path = extract_path(request)
-    # print(path)
-    str = extract_body(request,path)
-    if str == None:
-        client_socket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
-    else:
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(str)}\r\n\r\n{str}"
-        client_socket.send(response.encode("utf-8"))   
-
+    with socket.create_server(("localhost",4221)) as server_socket:
+        server_socket.listen() 
+        with cf.ThreadPoolExecutor() as executor:
+            while True:  
+                client_socket,addr = server_socket.accept()
+                executor.submit(handle_client, client_socket)
+                
 if __name__ == "__main__":
     main()
