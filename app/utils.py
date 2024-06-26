754594866618e1eeb42956c18c24856485046461
Indrajeet Thakur <38977906+thakur420@users.noreply.github.com>
2024-06-26 17:30:31 +0530

    single file code

--- app/utils.py
+++
@@ -1,8 +0,0 @@
-def extract_path(request):
-    request_lines = request.splitlines()
-    if request_lines:
-        request_line = request_lines[0]
-        parts = request_line.split()
-        if len(parts) > 1:
-            return parts[1]
-        return ""
